c4372db20477690efc2340b9a9bc1d343b3efc4f
William M Andre <William M Andre@DESKTOP-7S1TD07>
2019-11-18 15:21:50 +0100

    Initial commit

--- SistemaDeAeuroporto/src/Reserva.java
+++ SistemaDeAeuroporto/src/Reserva.java
@@ -1,13 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Mendes Apolo
- */
 public class Reserva {
-    
+
+    private String numReserva;
+    private Passageiro passageiro;
+    private Voo voo;
+    private String tipodereserva; //(1ra classe, executiva, económica)
+    private double preco;
+
+    public Reserva(String numReserva, Passageiro passageiro, Voo voo, String tipodereserva, double preco) {
+        this.numReserva = numReserva;
+        this.passageiro = passageiro;
+        this.voo = voo;
+        this.tipodereserva = tipodereserva;
+        this.preco = preco;
+    }
+
+    public String getNumReserva() {
+        return numReserva;
+    }
+
+    public void setNumReserva(String numReserva) {
+        this.numReserva = numReserva;
+    }
+
+    public Passageiro getPassageiro() {
+        return passageiro;
+    }
+
+    public void setPassageiro(Passageiro passageiro) {
+        this.passageiro = passageiro;
+    }
+
+    public Voo getVoo() {
+        return voo;
+    }
+
+    public void setVoo(Voo voo) {
+        this.voo = voo;
+    }
+
+    public String getTipodereserva() {
+        return tipodereserva;
+    }
+
+    public void setTipodereserva(String tipodereserva) {
+        this.tipodereserva = tipodereserva;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return getNumReserva();
+    }
+
 }
