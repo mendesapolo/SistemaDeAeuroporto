aa2b5983be69d300c4f9c75be4bdf632d86985c6
William M Andre <William M Andre@DESKTOP-7S1TD07>
2019-11-18 15:32:21 +0100

    Modelo da tabela Feito

---
+++ SistemaDeAeuroporto/src/Tabela_Reserva.java
@@ -0,0 +1,48 @@
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+public class Tabela_Reserva extends AbstractTableModel{
+
+    private ArrayList<Reserva> lista_reserva;
+    String colunas[] = {"Nº ", "Passageiro","Voo","Tipo","Preço"};
+
+    @Override
+    public String getColumnName(int column) {
+        return colunas[column];
+    }
+
+    @Override
+    public int getRowCount() {
+        return lista_reserva.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return this.lista_reserva.get(rowIndex).getNumReserva();
+            case 1:
+                return this.lista_reserva.get(rowIndex).getPassageiro();
+            case 2:
+                return this.lista_reserva.get(rowIndex).getVoo();
+            case 3:
+                return this.lista_reserva.get(rowIndex).getTipodereserva();
+            case 4:
+                return this.lista_reserva.get(rowIndex).getPreco();
+            default:
+                return null;
+        }
+    }
+
+    public void setListaReserva(ArrayList<Reserva> Lista) {
+        this.lista_reserva = Lista;
+        fireTableDataChanged();
+    }
+    
+}
